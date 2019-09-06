29ad75f5a5f5df1dae390c6872c256a0254b5b65
Mike Smith <mike.smith@sescon.co.uk>
2019-09-06 13:41:04 +0100

    No longer need application.properties after merge with master.

--- src/test/java/uk/gov/dhsc/htbhf/CucumberConfiguration.java
+++ src/test/java/uk/gov/dhsc/htbhf/CucumberConfiguration.java
@@ -2,12 +2,10 @@ package uk.gov.dhsc.htbhf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import uk.gov.dhsc.htbhf.browserstack.BrowserStackConfiguration;
 import uk.gov.dhsc.htbhf.local.LocalConfiguration;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @Import(value = {LocalConfiguration.class, BrowserStackConfiguration.class})
 public class CucumberConfiguration {
 
