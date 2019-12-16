c841c4a1de75edb53bfee6b18ad11d2f47262378
Matt Smith <matthew.smith@thinkinglogic.com>
2019-12-16 16:51:58 +0000

    HTBHF-2729 Added a failure scenario when unable to match children's dates of birth

--- src/test/java/uk/gov/dhsc/htbhf/steps/ClaimScenario.java
+++ src/test/java/uk/gov/dhsc/htbhf/steps/ClaimScenario.java
@@ -12,6 +12,7 @@ public enum ClaimScenario {
     IDENTITY_NOT_MATCHED("claim-identity-failed.json"),
     ELIGIBILITY_NOT_CONFIRMED("claim-eligibility-failed.json"),
     DUPLICATE_CLAIM("claim-duplicate.json"),
+    CHILD_DOB_MISMATCH("claim-child-dob-mismatch.json"),
     POSTCODE_MISMATCH("claim-postcode-mismatch.json"),
     FULL_ADDRESS_MISMATCH("claim-full-address-mismatch.json"),
     ADDRESS_LINE_ONE_MISMATCH("claim-address-line-one-mismatch.json");
